f3d0501023e6787a2e9f586f4e26e8e77848c431
Mario Nobrega <mario.cesarnobrega@gmail.com>
2023-07-24 14:19:22 -0300

    inicial commit

---
+++ scrip.js
@@ -0,0 +1,19 @@
+function togglemode()
+{
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    const img=document.querySelector("#profile img")
+
+
+    if (html.classList.contains("light"))
+    {
+        img.setAttribute("src", './aasents/avatar-light.png')
+    }
+    else
+    {
+        img.setAttribute("src",'/aasents/Avatar.png')
+    }
+
+    // pegga a tag img ,substituir a img 
+}
